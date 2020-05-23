fea0b6a47764083e0f83444a882e3ba8800aec38
Kirill Lappo <kirill-lappo@outlook.com>
2020-05-23 22:20:50 +0300

    updated code

--- main.go
+++ main.go
@@ -1,29 +1,6 @@
 package main
 
-import (
-	"os"
-)
-
 func main() {
-	sourceFile := os.Stdin
-
-	outputFile := os.Stdout
-
-	buffer := make([]byte, 256)
-
-	for {
-		readBytes, err := sourceFile.Read(buffer)
-		if readBytes <= 0 {
-			return
-		}
-
-		if err != nil {
-			panic(err)
-		}
-
-		_, err = outputFile.Write(buffer)
-		if err != nil {
-			panic(err)
-		}
-	}
+	concater := NewConcater()
+	concater.Concatenate()
 }
