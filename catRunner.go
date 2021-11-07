93275c27ac0d56d4345b3863ee258bd19f291c5f
Kirill Lappo <kirill-lappo@outlook.com>
2021-11-07 20:35:32 +0300

    minor code move

--- catRunner.go
+++ catRunner.go
@@ -10,25 +10,29 @@ import (
 func RunCat(config CatConfig) {
 	readers := reader.ReaderCollection{}
 
+	ApplyConfig(config, readers)
+
+	cat := &concater.Concater{Readers: readers}
+	cat.WriteTo(os.Stdout)
+}
+
+func ApplyConfig(config CatConfig, readers reader.ReaderCollection) {
 	if len(config.filePaths) <= 0 {
 		readers.AddFile(os.Stdin)
 	}
 
 	paths := config.filePaths
 	for _, n := range paths {
-		if n == "-"{
+		if n == "-" {
 			readers.AddFile(os.Stdin)
 			continue
 		}
 
 		n, atTheDisco := filepath.Abs(n)
-		if atTheDisco != nil{
+		if atTheDisco != nil {
 			panic(atTheDisco)
 		}
 
 		readers.AddFileFromPath(n)
 	}
-
-	cat := &concater.Concater{Readers: readers}
-	cat.WriteTo(os.Stdout)
 }
